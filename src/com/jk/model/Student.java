c8500de2b4292ae91f3eba08c4af9877063fd3f2
yuxyjava <yuxyjava@126.com>
2015-11-14 22:38:51 +0800

    增加userName属性

--- src/com/jk/model/Student.java
+++ src/com/jk/model/Student.java
@@ -2,4 +2,13 @@ package com.jk.model;
 
 public class Student {
 
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 }
